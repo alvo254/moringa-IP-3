79efd13af743d360f8c78220c249a6a2b1ae4df9
alvo254 <alvinkhamron@gmail.con>
2022-05-15 23:00:20 +0300

    question addditions

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+var questionBank= [
+    {
+        question : 'Whic of the following is a prgramming language',
+        option : ['Django','Python','CSS','XML'],
+        answer : 'Python'
+    },
+    {
+        question : 'In rust programming language how many ownership rules do we have?.',
+        option : ['As many as we want','1','3',],
+        answer : '3'
+    },
+    {
+        question : 'How do we use inherit in python?.',
+        option : ['Superclass(superset)','classA.inherit()','inherit(class)','All of the above'],
+        answer : 'Superclass(superset)'
+    },
+    {
+        question : 'What is HTML in full?.',
+        option : ['Cascading sheet','hypertext transfer protocal','all the above','hypertext markup language'],
+        answer : 'Hypertext markup language'
+    },
+    {
+        question : 'Which of the following can be used as a scripting languages',
+        option : ['C++','python','rust','html'],
+        answer : 'python'
+    }
+]
